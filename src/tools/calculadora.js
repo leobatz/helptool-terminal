23b0bbb2e513f409d135f639109077246ef3f476
miojo01 <bats.leco@gmail.com>
2026-08-17 20:44:07 -0300

    continuando calculadora function

--- src/tools/calculadora.js
+++ src/tools/calculadora.js
@@ -1,9 +1,27 @@
 import terminal from '../shortcuts/readlineInterface.js'
 import perguntar from '../shortcuts/functionPerguntar.js'
+import { select } from '@clack/prompts';
 
-terminal.question("Digite o primeiro número: ", (resposta) => {
-    console.log(`Olá, ${resposta}!`);
+async function calculadora() {
+    while (true) {
+        let primeiroNumero = await perguntar('Digite o primeiro número: ', terminal);
+        primeiroNumero = Number(primeiroNumero);
 
-    rl.close();
-});
+        if (Number.isNaN(primeiroNumero)) {
 
+        }
+    }
+
+    const opcaoOperador = await select({
+        message: 'Selecione o operador:',
+        options: [
+            { label: '+', value: 'soma'},
+            { label: '-', value: 'subtracao'},
+            { label: '/', value: 'divisao'},
+            { label: 'x', value: 'multiplicacao'}
+        ]
+    });
+
+    let segundoNumero = await perguntar('Digite o segundo número: ', terminal);
+
+}

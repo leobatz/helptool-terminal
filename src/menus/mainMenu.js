a9b3c67ccc8c77421efd2cfafd9cba0063c850fd
leobatz <bats.leco@gmail.com>
2026-08-18 17:31:41 -0300

    feat: reformulando mainMenu

--- src/menus/mainMenu.js
+++ src/menus/mainMenu.js
@@ -1,14 +1,23 @@
-import { select } from '@clack/prompts';
+import terminal from '../shortcuts/readlineInterface.js'
+import { perguntar } from '../shortcuts/functionPerguntar.js'
+import { calculadora } from '../tools/calculadora.js'
 
-export default function mainMenu() {
-    const opcao = await select({
-        message: 'Escolha uma ferramenta:',
-        options: [
-            { label: 'Calculadora',  value: 'calculadora' },
-        ],
-    });
+export default async function mainMenu() {
+    const opcao = await perguntar(`
+    ╔════════════════════════════════╗
+    ║          HELP TOOLS            ║
+    ╠════════════════════════════════╣
+    ║                                ║
+    ║  1 - Calculadora               ║
+    ║  2 - Conversor                 ║
+    ║  3 - Sair                      ║
+    ║                                ║
+    ╚════════════════════════════════╝
+    
+    Digite uma opção: `, terminal);
 
     switch(opcao) {
         case 'calculadora':
+            await calculadora();
     }
 }

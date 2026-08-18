38420d13ed55d281a41af9b5e42dae3131992b86
leobatz <bats.leco@gmail.com>
2026-08-18 17:32:10 -0300

    refactoring: reformulando calculadora

--- src/tools/calculadora.js
+++ src/tools/calculadora.js
@@ -1,43 +1,58 @@
 import terminal from '../shortcuts/readlineInterface.js'
-import perguntar from '../shortcuts/functionPerguntar.js'
-import { select } from '@clack/prompts';
+import { perguntar } from '../shortcuts/functionPerguntar.js'
 
-async function calculadora() {
+export async function calculadora() {
     while (true) {
         let primeiroNumero = await perguntar('Digite o primeiro número: ', terminal);
         primeiroNumero = Number(primeiroNumero);
 
         if (!Number.isNaN(primeiroNumero)) {
-            break
+            break;
         } else {
             console.log('Digite apenas números!');
         }
     }
 
-    const opcaoOperador = await select({
-        message: 'Selecione o operador:',
-        options: [
-            { label: '+', value: 'soma'},
-            { label: '-', value: 'subtracao'},
-            { label: '/', value: 'divisao'},
-            { label: 'x', value: 'multiplicacao'}
-        ]
-    });
+    terminal.close();
+
+    const opcaoOperador = await perguntar(`
+    ╔════════════════════════════════╗
+    ║          CALCULADORA           ║
+    ╠════════════════════════════════╣
+    ║                                ║
+    ║  1 - Soma (+)                  ║
+    ║  2 - Subtração (-)             ║
+    ║  3 - Divisão (/)               ║
+    ║  4 - Multiplicação (x)         ║
+    ║                                ║
+    ╚════════════════════════════════╝
+    Escolha o operador: `, terminal);
 
     while(true) {
         let segundoNumero = await perguntar('Digite o segundo número: ', terminal);
         segundoNumero = Number(segundoNumero);
 
         if (!Number.isNaN(segundoNumero)) {
-            break
+            break;
         } else {
             console.log('Digite apenas números!');
         }
     }
 
+    let resultadoCalculo;
+
     switch (opcaoOperador) {
         case 'soma':
-            const calculo = primeiroNumero + segundoNumero;
-            console.log()
+            resultadoCalculo = primeiroNumero + segundoNumero;
+            return console.log(`${primeroNumero} ${opcaoOperador} ${segundoNumero} = ${resultadoCalculo.toFixed(2)}`);
+        case 'subtracao':
+            resultadoCalculo = primeiroNumero - segundoNumero;
+            return console.log(`${primeroNumero} ${opcaoOperador} ${segundoNumero} = ${resultadoCalculo.toFixed(2)}`);
+        case 'divisao':
+            resultadoCalculo = primeiroNumero / segundoNumero;
+            return console.log(`${primeroNumero} ${opcaoOperador} ${segundoNumero} = ${resultadoCalculo.toFixed(2)}`);
+        case 'multiplicacao':
+            resultadoCalculo = primeiroNumero * segundoNumero;
+            return console.log(`${primeroNumero} ${opcaoOperador} ${segundoNumero} = ${resultadoCalculo.toFixed(2)}`);
     }
 }
